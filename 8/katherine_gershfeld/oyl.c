62526f01293fe74888ac3647ae921312be6c94d3
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-09-21 19:38:26 -0400

    2

--- 8/katherine_gershfeld/oyl.c
+++ 8/katherine_gershfeld/oyl.c
@@ -1,14 +1,14 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <math.h>
+//# include <math.h>
 
 // Problem 6
 int six(){
-  double s1 = 0.0;
-  double s2 = 0.0;
-  double i = 1;
+  int s1 = 0;
+  int s2 = 0;
+  int i = 1;
   while (i<=100){
-    s1+=pow(i,2.0);
+    s1+=i*i;
     s2+=i;
     i++;
   }
@@ -16,16 +16,25 @@ int six(){
   return s2-s1;
 }
 
-// Problem 20
-int twenty(){
-  
-}
-
-int sumDigits(){
-
+// Problem 2
+int two(){
+  int f1 = 1;
+  int f2 = 2;
+  int f = 0;
+  int sum = 2;
+  while (f<4000000){
+    f = f1 + f2;
+    if (f%2 == 0){
+      sum+=f;
+    }
+    f1 = f2;
+    f2 = f;
+  }
+  return sum;
 }
 
 int main(){
-  printf("%d",six());
+  printf("%d\n",six());
+  printf("%d\n",two());
   return 0;
 }
