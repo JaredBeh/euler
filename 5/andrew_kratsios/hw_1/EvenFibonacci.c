15a2a7d3b8739dd9f177a72f21e2ca4da0d7ac93
akratsios <akratsios18@gmail.com>
2015-09-21 19:15:57 -0400

    hw1

--- 5/andrew_kratsios/hw_1/EvenFibonacci.c
+++ 5/andrew_kratsios/hw_1/EvenFibonacci.c
@@ -3,8 +3,8 @@
 
 
 int sumEven(int x){
-	int c1 = 1;
-	int c2 = 0;
+	int c1 = 2;
+	int c2 = 1;
 	int sum = 0;
 	for ( int c1 ; c1 <= x;){
 		if (c1 % 2 == 0){
@@ -20,7 +20,7 @@ int sumEven(int x){
 
 int main(){
 
-	int x = sumEven(10);
+	int x = sumEven(4000000);
 	printf("%d\n", x);
 	return 0;
 
