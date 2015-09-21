6d512bb41387693ad5fa3348e2108aaec82dd56f
angelachan64 <angelachan64@gmail.com>
2015-09-21 13:53:04 -0400

    whee

--- 8/angela_chan/Work01/work01.c
+++ 8/angela_chan/Work01/work01.c
@@ -7,12 +7,27 @@ int summult35(){
     if (count%3==0||count%5==0){
       result+=count;
     } count++;
-  } printf("%d\n", result);
+  } printf("Sum of multiples of 3 and 5 under 1000: %d\n", result);
   return 0;
 }
 
-
+int sumfib(){
+  int result = 2;
+  int fib1 = 1;
+  int fib2 = 2;
+  int fib3;
+  while (fib2 < 4000000){
+    fib3 = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = fib3;
+    if(fib2%2 == 0){
+      result+=fib2;
+    }
+  } printf("Sum of even fibonacci numbers: %d\n", result);
+  return 0;
+}
 
 int main(){
   summult35();
+  sumfib();
 }
